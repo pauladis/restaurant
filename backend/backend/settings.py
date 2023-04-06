a87d77dbb482380a3f3591401c8c292c88f3f2e7
Raul Quinzani <raultq1@hotmail.com>
2023-04-06 10:18:33 -0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+#Yeah, I know that this key wasn't supposed to be here
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_5(1rx1g%2cwp@mlt&#z4-ecwk^#s9_)fpig7h-4q43clb87*&'
 
